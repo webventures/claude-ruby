23f86c37e7609a53680ad7ea53cfd4602749b16e
Matt @ Web Ventures <webven@mailgab.com>
2024-06-29 15:38:51 +1200

    Added constants for the main Claude models Added default user_message payload Added parse_response method Updated documentation

--- lib/claude/client.rb
+++ lib/claude/client.rb
@@ -3,6 +3,23 @@ require 'json'
 
 module Claude
   class Client
+    MODEL_CLAUDE_3_OPUS_20240229 = 'claude-3-opus-20240229'
+
+    MODEL_CLAUDE_3_SONNET_20240229 = 'claude-3-sonnet-20240229'
+    MODEL_CLAUDE_3_5_SONNET_20240620 = 'claude-3-5-sonnet-20240620'
+
+    MODEL_CLAUDE_3_HAIKU_20240307 = 'claude-3-haiku-20240307'
+
+    MODEL_CLAUDE_OPUS_LATEST = MODEL_CLAUDE_3_OPUS_20240229
+    MODEL_CLAUDE_SONNET_LATEST = MODEL_CLAUDE_3_5_SONNET_20240620
+    MODEL_CLAUDE_HAIKU_LATEST = MODEL_CLAUDE_3_HAIKU_20240307
+
+    MODEL_CLAUDE_FASTEST = MODEL_CLAUDE_HAIKU_LATEST
+    MODEL_CLAUDE_CHEAPEST = MODEL_CLAUDE_HAIKU_LATEST
+    MODEL_CLAUDE_BALANCED = MODEL_CLAUDE_SONNET_LATEST
+    MODEL_CLAUDE_SMARTEST = MODEL_CLAUDE_3_5_SONNET_20240620
+
+    MODEL_CLAUDE_DEFAULT = MODEL_CLAUDE_SONNET_LATEST
 
     def initialize(api_key)
       @api_key = api_key
@@ -10,15 +27,16 @@ module Claude
     end
 
     def messages(messages, params = {})
-      model = params[:model] || 'claude-3-opus-20240229'
-      max_tokens = params[:max_tokens] || 1024
+      model = params[:model] || MODEL_CLAUDE_DEFAULT
+      max_tokens = params[:max_tokens] || 4096
+      system = params[:system] || "You are a helpful assistant."
 
       url = "#{@endpoint}/messages"
 
       data = {
         model: model,
         messages: messages,
-        system: params[:system],
+        system: system,
         max_tokens: max_tokens,
         metadata: params[:metadata],
         stop_sequences: params[:stop_sequences],
@@ -37,6 +55,19 @@ module Claude
         'anthropic-version' => "2023-06-01",
         'x-api-key' => "#{@api_key}",
       }
+    end
+
+    def user_message(user_message)
+      [
+        {
+          "role": "user",
+          "content": user_message,
+        }
+      ]
+    end
+
+    def parse_response(response)
+      response['content'][0]['text']
     end
 
     private
